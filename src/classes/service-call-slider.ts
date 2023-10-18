48479eb2bbea90be0100872f1fe6215279b5f303
Nerwyn Singh <nerwyn1994@gmail.com>
2023-10-17 20:46:16 -0400

    move slider to it's own lit element, remove feature specific code out of main

---
+++ src/classes/service-call-slider.ts
@@ -0,0 +1,120 @@
+import { html, css, CSSResult } from 'lit';
+import { customElement } from 'lit/decorators.js';
+
+import { BaseServiceCallFeature } from './base-service-call-feature';
+
+@customElement('service-call-slider')
+export class ServiceCallSlider extends BaseServiceCallFeature {
+	constructor() {
+		super();
+	}
+
+	onInput(e: InputEvent) {
+		e.preventDefault();
+		e.stopImmediatePropagation();
+
+		const slider = e.currentTarget as HTMLInputElement;
+		let value = parseInt(slider.value) ?? 0;
+
+		if (value < 0) {
+			value = 0;
+		} else if (value > 100) {
+			value = 100;
+		}
+
+		// TODO - store this somewhere else
+		const start = slider.parentElement!.children[2].innerHTML ?? '50';
+		slider.value = start;
+
+		let i = parseInt(start);
+		const t = 1;
+		if (i > value) {
+			const id = setInterval(() => {
+				if (value >= i) {
+					clearInterval(id);
+				}
+				i -= 1;
+				slider.value = i.toString();
+			}, t);
+		} else if (i < value) {
+			const id = setInterval(() => {
+				if (value <= i) {
+					clearInterval(id);
+				}
+				i += 1;
+				slider.value = i.toString();
+			}, t);
+		}
+
+		// TODO - store this somewhere else
+		(
+			e.currentTarget as HTMLInputElement
+		).parentElement!.children[2].innerHTML = value.toString();
+	}
+
+	render() {
+		const slider = html`
+			<div class="slider-background"></div>
+			<input
+				type="range"
+				class="slider"
+				min="0"
+				max="100"
+				itemid=${this.itemid}
+				@input=${this.onInput}
+			/>
+			${this.renderLabel('50')}
+		`;
+
+		return slider;
+	}
+
+	static get styles() {
+		return [
+			super.styles as CSSResult,
+			css`
+				.slider-background {
+					position: absolute;
+					width: inherit;
+					height: inherit;
+					background: var(--slider-color);
+					opacity: 0.2;
+				}
+
+				.slider {
+					position: absolute;
+					appearance: none;
+					-webkit-appearance: none;
+					-moz-appearance: none;
+					height: inherit;
+					width: inherit;
+					overflow: hidden;
+					border-radius: 10px;
+					background: none;
+				}
+
+				.slider::-webkit-slider-thumb {
+					appearance: none;
+					-webkit-appearance: none;
+					height: 18px;
+					width: 4px;
+					border-radius: 12px;
+					background: #ffffff;
+					cursor: pointer;
+					box-shadow: calc(-100vw + 6px) 0 0 100vw var(--slider-color);
+				}
+
+				.slider::-moz-range-thumb {
+					appearance: none;
+					-moz-appearance: none;
+					height: 18px;
+					width: 4px;
+					border-radius: 12px;
+					background: #ffffff;
+					cursor: pointer;
+					box-shadow: calc(-100vw + 6px) 0 0 100vw var(--slider-color);
+				}
+			`,
+		];
+	}
+}
--- src/service-call-tile-feature.ts
+++ src/service-call-tile-feature.ts
@@ -7,6 +7,7 @@ import { HassEntity } from 'home-assistant-js-websocket';
 
 import { IConfig, IEntry } from './models/interfaces';
 import './classes/service-call-button';
+import './classes/service-call-slider';
 
 console.info(
 	`%c SERVICE-CALL-TILE-FEATURE v${version}`,
@@ -40,7 +41,7 @@ class ServiceCallTileFeature extends LitElement {
 		}
 		config = JSON.parse(JSON.stringify(config));
 
-		// Legacy config updates
+		// Rename buttons to entries
 		if ('buttons' in config && !('entries' in config)) {
 			(config as IConfig).entries = (
 				config as Record<'buttons', IEntry[]>
@@ -62,82 +63,6 @@ class ServiceCallTileFeature extends LitElement {
 		this.config = config;
 	}
 
-	renderIcon(icon: string, color?: string) {
-		let style = ``;
-		if (color) {
-			style = `color: ${color};`;
-		}
-		return html`<ha-icon .icon=${icon} style="${style}"></ha-icon>`;
-	}
-
-	renderLabel(text: string, color?: string) {
-		let style = ``;
-		if (color) {
-			style = `color: ${color};`;
-		}
-		return html`<div class="label" style="${style}">${text}</div>`;
-	}
-
-	onSlide(e: InputEvent) {
-		e.preventDefault();
-		e.stopImmediatePropagation();
-
-		const slider = e.currentTarget as HTMLInputElement;
-		let value = parseInt(slider.value) ?? 0;
-
-		if (value < 0) {
-			value = 0;
-		} else if (value > 100) {
-			value = 100;
-		}
-
-		// TODO - store this somewhere else
-		const start = slider.parentElement!.children[2].innerHTML;
-		slider.value = start;
-
-		let i = parseInt(start);
-		const t = 1;
-		if (i > value) {
-			const id = setInterval(() => {
-				if (value >= i) {
-					clearInterval(id);
-				}
-				i -= 1;
-				slider.value = i.toString();
-			}, t);
-		} else if (i < value) {
-			const id = setInterval(() => {
-				if (value <= i) {
-					clearInterval(id);
-				}
-				i += 1;
-				slider.value = i.toString();
-			}, t);
-		}
-
-		// TODO - store this somewhere else
-		(
-			e.currentTarget as HTMLInputElement
-		).parentElement!.children[2].innerHTML = value.toString();
-	}
-
-	renderSlider(itemid: number, _entry: IEntry) {
-		const slider = html`
-			<div class="slider-background"></div>
-			<input
-				type="range"
-				class="slider"
-				min="0"
-				max="100"
-				itemid=${itemid}
-				@input=${this.onSlide}
-			/>
-			${this.renderLabel('50')}
-		`;
-
-		return slider;
-	}
-
 	render() {
 		if (!this.config || !this.hass || !this.stateObj) {
 			return null;
@@ -145,8 +70,6 @@ class ServiceCallTileFeature extends LitElement {
 
 		const entries: TemplateResult[] = [];
 		for (const [itemid, entry] of this.config.entries.entries()) {
-			let renderedEntry: TemplateResult;
-
 			// Set entity ID to tile card entity ID if no other ID is present
 			if (
 				!('entity_id' in entry.data!) &&
@@ -159,19 +82,23 @@ class ServiceCallTileFeature extends LitElement {
 			const entryType = entry.type;
 			switch (entryType.toLowerCase()) {
 				case 'slider':
-					renderedEntry = this.renderSlider(itemid, entry);
 					entries.push(
-						html`<div class="container">${renderedEntry}</div>`,
+						html`<service-call-slider
+							.hass=${this.hass}
+							.entry${entry}
+							.itemid=${itemid}
+						/>`,
 					);
 					break;
 				case 'button':
 				default:
-					renderedEntry = html` <service-call-button
-						.hass=${this.hass}
-						.entry=${entry}
-						.itemid=${itemid}
-					/>`;
-					entries.push(renderedEntry);
+					entries.push(
+						html`<service-call-button
+							.hass=${this.hass}
+							.entry=${entry}
+							.itemid=${itemid}
+						/>`,
+					);
 					break;
 			}
 		}
@@ -190,105 +117,6 @@ class ServiceCallTileFeature extends LitElement {
 				padding: 0 12px 12px;
 				gap: 12px;
 				width: auto;
-			}
-			.container {
-				display: flex;
-				flex-flow: column;
-				place-content: center space-evenly;
-				align-items: center;
-				position: relative;
-				height: 40px;
-				width: 100%;
-				border-radius: 10px;
-				border: none;
-				padding: 0px;
-				box-sizing: border-box;
-				line-height: 0;
-				outline: 0px;
-				overflow: hidden;
-				font-size: inherit;
-				color: inherit;
-			}
-			.button {
-				background-color: var(--disabled-color);
-				opacity: 0.2;
-				transition: background-color 180ms ease-in-out;
-				position: absolute;
-				cursor: pointer;
-				height: inherit;
-				width: inherit;
-				border-radius: 10px;
-				border: none;
-			}
-			@media (hover: hover) {
-				.button:hover {
-					opacity: 0.3;
-				}
-			}
-			.button:active {
-				opacity: 0.3;
-			}
-
-			.slider-background {
-				position: absolute;
-				width: inherit;
-				height: inherit;
-				background: var(--slider-color);
-				opacity: 0.2;
-			}
-
-			.slider {
-				position: absolute;
-				appearance: none;
-				-webkit-appearance: none;
-				-moz-appearance: none;
-				height: inherit;
-				width: inherit;
-				overflow: hidden;
-				border-radius: 10px;
-				background: none;
-			}
-
-			.slider::-webkit-slider-thumb {
-				appearance: none;
-				-webkit-appearance: none;
-				height: 18px;
-				width: 4px;
-				border-radius: 12px;
-				background: #ffffff;
-				cursor: pointer;
-				box-shadow: calc(-100vw + 6px) 0 0 100vw var(--slider-color);
-			}
-
-			.slider::-moz-range-thumb {
-				appearance: none;
-				-moz-appearance: none;
-				height: 18px;
-				width: 4px;
-				border-radius: 12px;
-				background: #ffffff;
-				cursor: pointer;
-				box-shadow: calc(-100vw + 6px) 0 0 100vw var(--slider-color);
-			}
-
-			ha-icon {
-				position: relative;
-				pointer-events: none;
-				display: inline-flex;
-				flex-flow: column;
-				place-content: center;
-			}
-
-			.label {
-				position: relative;
-				pointer-events: none;
-				display: inline-flex;
-				justify-content: center;
-				align-items: center;
-				height: 15px;
-				width: inherit;
-				font-family: inherit;
-				font-size: 12px;
 			}
 		`;
 	}
