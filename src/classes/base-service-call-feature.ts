4e90ba540886454b6575e666ee83d051511a3d23
Nerwyn Singh <nerwyn1994@gmail.com>
2023-10-20 00:11:13 -0400

    add check for this.entry before any subkeys (which shouldn't be necessary so wtf)

--- src/classes/base-service-call-feature.ts
+++ src/classes/base-service-call-feature.ts
@@ -17,7 +17,7 @@ export class BaseServiceCallFeature extends LitElement {
 	}
 
 	set hass(hass: HomeAssistant) {
-		if (this.entry.value_attribute) {
+		if (this.entry && this.entry.value_attribute) {
 			if (this.entry.value_attribute == 'state') {
 				this.value = hass.states[this.entity_id].state;
 			} else {
