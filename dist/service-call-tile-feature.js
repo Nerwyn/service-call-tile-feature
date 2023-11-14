6a6070ed0774d97eb03e5e35c023c757117efaba
Nerwyn Singh <nerwyn1994@gmail.com>
2023-11-14 01:38:09 -0500

    alternate approach to default slider thumb

--- dist/service-call-tile-feature.js
+++ dist/service-call-tile-feature.js
@@ -129,33 +129,35 @@
 				.slider::-webkit-slider-thumb {
 					appearance: none;
 					-webkit-appearance: none;
-					height: 40px;
-					width: 24px;
+					height: 30px;
+					width: 12px;
 					border-style: solid;
-					border-width: 10px 4px 10px 16px;
-					border-radius: 10px;
+					border-width: 4px;
+					border-radius: 12px;
 					border-color: var(--slider-color);
 					background: #ffffff;
 					cursor: pointer;
 					opacity: var(--slider-opacity);
-					box-shadow: calc(-100vw - 6px) 0 0 calc(100vw - 8px)
-						var(--slider-color);
+					box-shadow:
+						calc(-100vw - 6px) 0 0 100vw var(--slider-color),
+						-4px 0 0 6px var(--slider-color);
 				}
 
 				.slider::-moz-range-thumb {
 					appearance: none;
 					-moz-appearance: none;
-					height: 40px;
-					width: 24px;
+					height: 30px;
+					width: 12px;
 					border-style: solid;
-					border-width: 10px 4px 10px 16px;
-					border-radius: 10px;
+					border-width: 4px;
+					border-radius: 12px;
 					border-color: var(--slider-color);
 					background: #ffffff;
 					cursor: pointer;
 					opacity: var(--slider-opacity);
-					box-shadow: calc(-100vw - 6px) 0 0 calc(100vw - 8px)
-						var(--slider-color);
+					box-shadow:
+						calc(-100vw - 6px) 0 0 100vw var(--slider-color),
+						-4px 0 0 6px var(--slider-color);
 				}
 
 				.slider-line-thumb::-webkit-slider-thumb {
