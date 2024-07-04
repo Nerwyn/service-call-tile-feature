0b0f1cfa6f58a63051bef33f9b534fae08472e89
Nerwyn Singh <nerwyn1994@gmail.com>
2024-07-04 00:14:41 -0400

    add tabs.js and primary-tab.js to webpack externals

--- webpack.config.js
+++ webpack.config.js
@@ -28,5 +28,10 @@ module.exports = {
 		],
 	},
 	devtool: env == 'production' ? false : 'eval',
-	externals: [/^@material\/web\/tabs\/(tabs|primary-tab)\.js/],
+	// externals: [/^@material\/web\/tabs\/(tabs|primary-tab)\.js/],
+	externals: {
+		'@material/web/tabs/tabs.js': '@material/web/tabs/tabs.js',
+		'@material/web/tabs/primary-tab.js':
+			'@material/web/tabs/primary-tab.js',
+	},
 };
