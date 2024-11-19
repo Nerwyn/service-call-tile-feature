5d0ca61844839a063e1f7a594881a7b08692bda1
Nerwyn Singh <nerwyn1994@gmail.com>
2024-11-18 23:02:31 -0500

    change dropdown to use divs and custom element options instead of select and option elements

--- src/classes/custom-feature-dropdown.ts
+++ src/classes/custom-feature-dropdown.ts
@@ -1,10 +1,13 @@
 import { css, CSSResult, html } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { customElement, state } from 'lit/decorators.js';
 
+import { IEntry } from '../models/interfaces';
 import { BaseCustomFeature } from './base-custom-feature';
 
 @customElement('custom-feature-dropdown')
 export class CustomFeatureDropdown extends BaseCustomFeature {
+	@state() showDropdown: boolean = false;
+
 	onStart(e: MouseEvent | TouchEvent) {
 		clearTimeout(this.renderRippleOff);
 		clearTimeout(this.renderRippleOn);
@@ -21,10 +24,11 @@ export class CustomFeatureDropdown extends BaseCustomFeature {
 
 	onEnd(e: MouseEvent | TouchEvent) {
 		if (!this.swiping) {
-			clearTimeout(this.getValueFromHassTimer);
-			this.getValueFromHass = false;
-			this.value = (e.currentTarget as HTMLElement).id;
-			this.resetGetValueFromHass();
+			// clearTimeout(this.getValueFromHassTimer);
+			// this.getValueFromHass = false;
+			// this.value = (e.currentTarget as HTMLElement).id;
+			// this.resetGetValueFromHass();
+			this.showDropdown = !this.showDropdown;
 			this.toggleRipple();
 		}
 	}
@@ -34,31 +38,51 @@ export class CustomFeatureDropdown extends BaseCustomFeature {
 
 		const dropdownOptions = [];
 		const options = this.config.options ?? [];
+		let selectedOption: IEntry | undefined = undefined;
 		for (const option of options) {
-			option.haptics = option.haptics ?? this.config.haptics;
-			dropdownOptions.push(
-				html`<option value=${this.renderTemplate(option.option ?? '')}>
-					<custom-feature-dropdown-option
-						.hass=${this.hass}
-						.config=${option}
-						.stateObj=${this.stateObj}
-					/>
-				</option>`,
-			);
-		}
-		const dropdown = html`<select
-			@mousedown=${this.onMouseDown}
-			@mouseup=${this.onMouseUp}
-			@mousemove=${this.onMouseMove}
-			@touchstart=${this.onTouchStart}
-			@touchend=${this.onTouchEnd}
-			@touchmove=${this.onTouchMove}
-			@contextmenu=${this.onContextMenu}
-		>
-			${dropdownOptions}
-		</select>`;
+			const optionName = this.renderTemplate(option.option as string);
+			if (
+				this.value != undefined &&
+				(this.value ?? '').toString() == (optionName ?? '').toString()
+			) {
+				selectedOption = option;
+			}
 
-		return html`${this.buildBackground()}${dropdown}${this.buildRipple()}${this.buildStyles()}`;
+			option.haptics = option.haptics ?? this.config.haptics;
+			dropdownOptions.push(html`
+				<custom-feature-dropdown-option
+					.hass=${this.hass}
+					.config=${option}
+					.stateObj=${this.stateObj}
+					id=${optionName}
+				/>
+			`);
+		}
+		const dropdown = this.showDropdown
+			? html`<div class="dropdown">${options}</div>`
+			: '';
+
+		const select = html` <div class="select">
+			<div
+				class="background"
+				@mousedown=${this.onMouseDown}
+				@mouseup=${this.onMouseUp}
+				@mousemove=${this.onMouseMove}
+				@touchstart=${this.onTouchStart}
+				@touchend=${this.onTouchEnd}
+				@touchmove=${this.onTouchMove}
+				@contextmenu=${this.onContextMenu}
+			>
+				${this.buildRipple()}
+			</div>
+			${selectedOption
+				? html`${this.buildIcon(selectedOption)}${this.buildLabel(
+						selectedOption,
+					)}`
+				: ''}
+		</div>`;
+
+		return html`${this.buildBackground()}${select}${dropdown}${this.buildStyles()}`;
 	}
 
 	updated() {
@@ -105,16 +129,17 @@ export class CustomFeatureDropdown extends BaseCustomFeature {
 						var(--ha-ripple-color, var(--secondary-text-color))
 					);
 				}
-
-				select {
-					background: 0 0 !important;
-					opacity: 1 !important;
+				.select {
+					display: flex;
+					flex-direction: row;
+					align-items: center;
+					gap: 10px;
+					padding: 6px 10px;
+				}
+				.down-arrow {
 					position: absolute;
-					cursor: pointer;
-					height: 100%;
-					width: 100%;
-					border: none;
-					overflow: hidden;
+					right: 0px;
+					--mdc-icon-size: 20px;
 				}
 			`,
 		];
@@ -123,8 +148,38 @@ export class CustomFeatureDropdown extends BaseCustomFeature {
 
 @customElement('custom-feature-dropdown-option')
 export class CustomFeatureDropdownOption extends BaseCustomFeature {
+	onStart(_e: MouseEvent | TouchEvent) {
+		clearTimeout(this.renderRippleOff);
+		clearTimeout(this.renderRippleOn);
+		this.renderRipple = true;
+		this.swiping = false;
+		this.fireHapticEvent('light');
+	}
+
+	onEnd(_e: MouseEvent | TouchEvent) {
+		if (!this.swiping) {
+			this.sendAction('tap_action');
+			this.toggleRipple();
+		}
+	}
+
+	onMouseLeave(_e: MouseEvent) {
+		this.endAction();
+		this.swiping = true;
+		this.toggleRipple();
+	}
+
+	onTouchCancel(_e: TouchEvent) {
+		this.toggleRipple();
+	}
+
 	render() {
-		return html`${this.buildBackground()}${this.buildIcon()}${this.buildLabel()}${this.buildStyles()}`;
+		this.setValue();
+
+		return html`<div class="container">
+				${this.buildBackground()}${this.buildIcon()}${this.buildLabel()}${this.buildRipple()}
+			</div>
+			${this.buildStyles()}`;
 	}
 
 	static get styles() {
@@ -132,6 +187,10 @@ export class CustomFeatureDropdownOption extends BaseCustomFeature {
 			super.styles as CSSResult,
 			css`
 				:host {
+					height: var(--mdc-menu-item-height, 48px);
+					width: 100%;
+				}
+				.background {
 				}
 			`,
 		];
