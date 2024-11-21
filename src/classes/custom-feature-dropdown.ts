f2dab7c4fa3e84df10d451d58be1bc5846ad188f
Nerwyn Singh <nerwyn1994@gmail.com>
2024-11-20 21:02:45 -0500

    remove dropdown properties before showDropdown check instead of if false, fix remove overflow-y

--- src/classes/custom-feature-dropdown.ts
+++ src/classes/custom-feature-dropdown.ts
@@ -143,6 +143,11 @@ export class CustomFeatureDropdown extends BaseCustomFeature {
 		const dropdown = this.shadowRoot?.querySelector(
 			'.dropdown',
 		) as HTMLElement;
+		dropdown.style.removeProperty('left');
+		dropdown.style.removeProperty('top');
+		dropdown.style.removeProperty('bottom');
+		dropdown.style.removeProperty('max-height');
+		dropdown.style.removeProperty('overflow-y');
 		if (this.showDropdown) {
 			const rect = this.getBoundingClientRect();
 			const edgeOffset = 48;
@@ -168,14 +173,6 @@ export class CustomFeatureDropdown extends BaseCustomFeature {
 				`${window.innerHeight - rect.bottom - edgeOffset}px`,
 			);
 			dropdown.style.setProperty('overflow-y', 'scroll');
-		} else {
-			setTimeout(() => {
-				dropdown.style.removeProperty('left');
-				dropdown.style.removeProperty('top');
-				dropdown.style.removeProperty('bottom');
-				dropdown.style.removeProperty('max-height');
-				dropdown.style.removeProperty('overscroll-y');
-			}, 150);
 		}
 	}
 
